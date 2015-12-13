93bdada1948c5eaa72343b39ed8cad0c97066ec4
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-12-13 15:26:22 +0000

    WeakRef class

---
+++ Snippets/WeakRef.swift
@@ -0,0 +1,44 @@
+// Snippets WeakRef.swift
+//
+// Copyright © 2015, Roy Ratcliffe, Pioneering Software, United Kingdom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+import Foundation
+
+/// Encapsulates a weak reference to an object. You can strongly retain this
+/// object when you cannot weakly retain its object directly, such as associated
+/// objects.
+public class WeakRef: NSObject {
+
+  /// Weak reference to an object. Weak references must be optionals. At some
+  /// unspecified point in the future, a non-nil weak reference can become nil;
+  /// the unwrapped optional answers nil.
+  ///
+  /// You cannot use `weak let` in Swift 2. All weak object references must be
+  /// mutable. That makes sense. The optional can become `nil`.
+  public weak var object: NSObject?
+
+  public init(object: NSObject) {
+    self.object = object
+  }
+
+}
