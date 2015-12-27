0fb9be0dc6c5c43a9e32a6032dcd32d3fe5459d1
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-12-27 12:04:38 +0100

    UIColor.fromHTML(string) implementation

---
+++ Snippets/UIColor+Snippets.swift
@@ -0,0 +1,76 @@
+// Snippets UIColor+Snippets.swift
+//
+// Copyright © 2015, Roy Ratcliffe, Pioneering Software, United Kingdom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+import UIKit
+
+extension UIColor {
+
+  /// - returns: a UIColor based on a HTML colour string. Answers `nil` if the
+  ///   given string cannot convert to a colour because there are not enough, or
+  ///   too many, hexadecimal digits.
+  ///
+  /// Implementation uses a regular expression to extract all the hexadecimal
+  /// digits, ignoring hash prefixes and whitespace if any. Aborts if the
+  /// regular expression for a hexadecimal digit fails to compile.
+  public class func fromHTML(string: String) -> UIColor? {
+    guard let expression = try? NSRegularExpression(pattern: "[0-9a-f]", options: .CaseInsensitive) else {
+      abort()
+    }
+    let range = NSMakeRange(0, string.characters.count)
+    let matches = expression.matchesInString(string, options: [], range: range)
+    let digits = matches.map { (match) -> String in
+      (string as NSString).substringWithRange(match.range)
+    }
+    var digitPairs: [String]
+    switch digits.count {
+    case 3, 4:
+      // rgb, argb
+      digitPairs = digits.map { (digit) -> String in
+        digit + digit
+      }
+    case 6, 8:
+      // rrggbb, aarrggbb
+      digitPairs = (0..<(digits.count >> 1)).map({ (index) -> String in
+        digits[index << 1] + digits[(index << 1) + 1]
+      })
+    default:
+      return nil
+    }
+    let components = digitPairs.map { (digitPair) -> CGFloat in
+      var result: UInt32 = 0;
+      // Ignore the fact that the scanner can answer false. It never will
+      // because we already know that the scanner will always see hexadecimal,
+      // nothing more, nothing less.
+      NSScanner(string: digitPair).scanHexInt(&result)
+      return CGFloat(result) / 255.0
+    }
+    var index = 0
+    let alpha = components.count == 4 ? components[index++] : 1.0
+    let red = components[index++]
+    let green = components[index++]
+    let blue = components[index]
+    return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+  }
+
+}
